eed38362483ea27d7b4234a74867ca29f5125c76
ThaUnknown <6506529+ThaUnknown@users.noreply.github.com>
2022-12-09 21:51:51 +0100

    fix: better exports

--- node.js
+++ node.js
@@ -1,3 +1,30 @@
-import polyfill from 'node-fetch'
+import _fetch, {
+  Blob as _Blob,
+  File as _File,
+  FormData as _FormData,
+  Headers as _Headers,
+  Request as _Request,
+  Response as _Response
+} from 'node-fetch'
 
-export default global.fetch || polyfill
+export const fetch = global.fetch || _fetch
+export default fetch
+
+export const Blob = global.Blob || _Blob
+export const File = global.File || _File
+export const FormData = global.FormData || _FormData
+export const Headers = global.Headers || _Headers
+export const Request = global.Request || _Request
+export const Response = global.Response || _Response
+export const AbortController = global.AbortController
+export const AbortSignal = global.AbortSignal
+
+export {
+  AbortError,
+  FetchError,
+  blobFrom,
+  blobFromSync,
+  fileFrom,
+  fileFromSync,
+  isRedirect
+} from 'node-fetch'
